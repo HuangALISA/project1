4f613f0d56c6f81ac7f04596c98d2d80d8c531b8
Alisa Suslova <suslova.alisa@yandex.ru>
2022-07-25 17:26:29 +0300

    First commit

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React from "react"
+const Loader = () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: "5rem" }}>
+    <div class="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+)
+export default Loader
